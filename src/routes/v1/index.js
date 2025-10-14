fde3c62e13e8b88b135444b60e45bf9cd6fb9113
amShubhama <shubham14sk32@gmail.com>
2025-10-14 18:10:31 +0530

    cleanup: remove unused route from booking routes

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,6 +4,5 @@ const { BookingController } = require('../../controllers/index');
 const router = express.Router();
 
 router.post('/bookings', BookingController.create);
-router.get('/bookings', (req, res) => res.send('Working'));
 
 module.exports = router;
