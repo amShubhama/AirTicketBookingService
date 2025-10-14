785d93393c411e229e1926845e638063fb4fe9c6
amShubhama <shubham14sk32@gmail.com>
2025-10-14 11:49:01 +0530

    chore: initial project setup

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { PORT, DB_SYNC } = require('./config/serverConfig');
+const bodyParser = require('body-parser');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
+const app = express();
+
+
+
+const setupAndStartServer = () => {
+
+    //middlewares
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    //API's for health-check
+    app.get('/', (req, res) => {
+        res.send('Server is working');
+    })
+
+    //API's Routes
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`Server start on PORT ${PORT}`);
+
+        if (DB_SYNC) {
+            console.log(DB_SYNC);
+            db.sequelize.sync({ alter: true });
+        }
+    })
+}
+
+setupAndStartServer();
